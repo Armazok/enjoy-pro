5b16b3bfdcb6d74e43c5b52d0a7fb58c0c7fe3e9
Armazok <nikolai.borsin@mail.ru>
2026-02-12 15:02:37 +0300

    chore: настройка tanstack v4

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, Suspense } from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+// eslint-disable-next-line no-restricted-imports
+import { ErrorBoundary } from '@app/providers';
 
 import App from './App';
 
+const queryClient = new QueryClient();
+
 render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<ErrorBoundary fallback={<div>Oops!</div>}>
+					<Suspense fallback={<div>Loading...</div>}>
+						<App />
+					</Suspense>
+				</ErrorBoundary>
+				<ReactQueryDevtools initialIsOpen={true} />
+			</QueryClientProvider>
+		</BrowserRouter>
 	</StrictMode>,
 	document.getElementById('root'),
 );
