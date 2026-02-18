01acce602749d9103d8c38b5a3cfcfebfd62a7e1
Armazok <nikolai.borsin@mail.ru>
2026-02-18 13:20:44 +0300

    fix: бесконечный рендер

--- src/entities/auth/hook/useAuthStorageWatcher.ts
+++ src/entities/auth/hook/useAuthStorageWatcher.ts
@@ -1,23 +1,29 @@
 import { useEffect } from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 import { ROUTES } from '@shared/constant';
 
 import { authStorage } from '../lib/auth-storage';
 
 export const useAuthStorageWatcher = () => {
+	const location = useLocation();
+
 	useEffect(() => {
 		const handleStorage = (e: StorageEvent) => {
 			if (e.key === authStorage.TOKEN_KEY && !e.newValue) {
-				window.location.href = ROUTES.LOGIN;
+				if (location.pathname !== ROUTES.LOGIN) {
+					window.location.href = ROUTES.LOGIN;
+				}
 			}
 		};
 
 		window.addEventListener('storage', handleStorage);
 
-		if (!authStorage.getToken()) {
+		if (!authStorage.getToken() && location.pathname !== ROUTES.LOGIN) {
 			window.location.href = ROUTES.LOGIN;
 		}
 
 		return () => window.removeEventListener('storage', handleStorage);
-	}, []);
+	}, [location.pathname]);
 };
